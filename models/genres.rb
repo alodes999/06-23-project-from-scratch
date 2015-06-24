92928270806cd1e1115eae33b83470796bbce101
Andrew Lodes <alodes999@gmail.com>
2015-06-24 12:03:34 -0500

    Add documentation to genre.rb and genre_change.erb

--- models/genres.rb
+++ models/genres.rb
@@ -7,12 +7,19 @@ class Genre
   include DatabaseInstanceMethods
 
   attr_accessor :id, :name
-
+  # This is our initialize method for my Genre class(model).  We have two attributes, the id, an Integer
+  # corresponding to the row in our DB table, and name, the name of our genre, as a String
+  # 
+  # We have 1 optional argument, a Hash, that can contain an id or name.
   def initialize(options={})
     @id = options["id"]
     @name = options["name"]
   end
-
+  # This class method allows us to list all of the games associated with the given Genre.
+  # 
+  # This accepts one argument, the id of the genre we want to look up
+  # 
+  # Returns an Array of Game class Objects that belong to the genre looked up
   def self.games_in_genre(id)
     gameslist = CONNECTION.execute("SELECT * FROM games WHERE genre_id = #{id};")
     genre_array = []
