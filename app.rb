9e8a3984e36c0a97945874f4fe55a671bf464cc1
Andrew Lodes <alodes999@gmail.com>
2015-06-25 09:38:22 -0500

    Add views for user, update controller for user

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ require_relative 'database_setup'
 
 require_relative 'models/genres.rb'
 # require_relative 'models/games.rb'
-# require_relative 'models/users.rb'
+ require_relative 'models/users.rb'
 # require_relative 'models/reviews.rb'
 
 require_relative 'controllers/home.rb'
 require_relative 'controllers/genre_control.rb'
 # require_relative 'controllers/game_control.rb'
-# require_relative 'controllers/user_control.rb'
+require_relative 'controllers/user_control.rb'
 # require_relative 'controllers/review_control.rb'
--- controllers/user_control.rb
+++ controllers/user_control.rb
@@ -3,17 +3,29 @@ get "/user" do
 end
 
 get "/users_read" do
-  
+  erb :'/user/user_read'
 end
 
 get "/users_add" do
-  
+  erb :'/user/user_add'
 end
 
 get "/users_change" do
-  
+  erb :'/user/user_change'
 end
 
 get "/users_delete" do
+  erb :'/user/user_delete'
+end
+
+get "/user_add_to_database" do
+  
+end
+
+get "/user_change_in_database" do
+  
+end
+
+get "/user_delete_from_database" do
   
 end
