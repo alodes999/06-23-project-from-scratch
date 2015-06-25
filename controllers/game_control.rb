676acce3e062a4f002732aed4d349f7cca3cdbff
Andrew Lodes <alodes999@gmail.com>
2015-06-25 11:34:17 -0500

    Add views listeners to game_control
    Add some content to game_main.erb and a couple placeholder pages

---
+++ controllers/game_control.rb
@@ -0,0 +1,19 @@
+get "/games_read" do
+  erb :'game/game_read'
+end
+
+get "/games_add" do
+  erb :'game/game_add'
+end
+
+get "/games_change" do
+  erb :'game/game_change'
+end
+
+get "/games_delete" do
+  erb :'game/game_delete'
+end
+
+get "/games_reviews" do
+  erb :'game/games_reviews'
+end
