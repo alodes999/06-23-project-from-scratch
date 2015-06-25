d0accf9c76bd0a15b337ad54c39bc3d337d3be2a
Andrew Lodes <alodes999@gmail.com>
2015-06-25 16:18:37 -0500

    Add delete functionality for game_control

--- controllers/game_control.rb
+++ controllers/game_control.rb
@@ -21,3 +21,18 @@ end
 get "/games_reviews" do
   erb :'game/game_reviews'
 end
+# This listener pulls from the game_delete.erb page.  It grabs the param game[delete_id], an ID
+# of the row we want to delete in the games table.  It then deletes the row.
+# 
+# Once we add the games table, it will check to see if the game has reviews attached.  If it
+# does not, the delete goes through and the user is sent to the Success page.
+# 
+# If reviews are attached, it sends the user to the Error page.
+get '/game_delete_from_database' do
+#  if Game.reviews_for_game(params["game_to_delete"]) == []
+    Game.delete(params["game"]["delete_id"])
+    erb :"success/data_deleted"
+#  else
+#    erb :"error/data_exists"
+#  end
+end
