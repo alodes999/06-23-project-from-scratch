b32463f7bd2176b862caf26420a8b2a29311aa0c
Andrew Lodes <alodes999@gmail.com>
2015-06-25 13:46:31 -0500

    Create views for medium model and controller

--- database_setup.rb
+++ database_setup.rb
@@ -2,7 +2,7 @@ CONNECTION = SQLite3::Database.new("gamereviewrepos.db")
 
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS genres (id INTEGER PRIMARY KEY, name TEXT);")
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT);")
-# CONNECTION.execute("CREATE TABLE IF NOT EXISTS mediums (id INTEGER PRIMARY KEY, name TEXT);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS mediums (id INTEGER PRIMARY KEY, name TEXT);")
 # CONNECTION.execute("CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY, name TEXT, genres_id INTEGER, mediums_id INTEGER, FOREIGN KEY(genres_id) REFERENCES genres(id), FOREIGN KEY(mediums_id) REFERENCES mediums(id) );")
 # CONNECTION.execute("CREATE TABLE IF NOT EXISTS reviews (id INTEGER PRIMARY KEY, games_id INTEGER, users_id INTEGER, review_score INTEGER, FOREIGN KEY(games_id) REFERENCES games(id), FOREIGN KEY(users_id) REFERENCES users(id) );")
 
