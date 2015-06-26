68a9f427095821411ef64fa3a5f05e8eca023382
Andrew Lodes <alodes999@gmail.com>
2015-06-25 21:52:33 -0500

    Add change functionality to review

--- controllers/review_control.rb
+++ controllers/review_control.rb
@@ -12,11 +12,35 @@ get "/review_add" do
   erb :'/review/review_add'
 end
 # This listener pulls from the review main page, and sends the review to the review_change page.
-# This page allows the user to pick which review to modify.  Once there, the user can pick to
-# change the game or user the review attached to, or the reviews score.  Once submitted,
-# it is sent to the reviews_change_to_database route.
+# This page allows the user to pick which review to modify.  That sends the user back to the
+# "review_change_input" route.
 get "/review_change" do
   erb :'/review/review_change'
+end
+
+get "review_change_input" do
+  @change_review_pick = Review.find(params["review"]["id"])
+  erb :'/review/review_change_action'
+end
+# This listener pulls from the review_change_action.erb page.  It pushes the params["review"]
+# hash the form returns into a variable, checks to see if the score was empty, and passes the
+# changed values into an instantiated Review Object to update the values.  The updated Object
+# has its' .save method called to send the values back to the Database row it belongs to.
+get "/review_change_in_database" do
+  change_hash = params["review"]
+  if change_hash["score"].empty?
+    erb :"error/no_data_in_field"
+  else
+    review_to_change = Review.find(params["review"]["id"])
+  
+    review_to_change.score = change_hash["score"]
+    review_to_change.users_id = change_hash["users_id"]
+    review_to_change.games_id = change_hash["games_id"]
+  
+    review_to_change.save
+  
+    erb :"success/data_changed"
+  end
 end
 # This listener pulls from the review main page, and sends the user to the review_delete page.
 # This page allows the user to delete a review.
@@ -47,9 +71,9 @@ end
 get "/review_change_in_database" do
   change_review = Review.find(params["review"]["change_id"])
   
-  change_review.users_id = params["review"]["users_id"]
-  change_review.games_id = params["review"]["games_id"]
-  change_review.score = params["review"]["score"]
+  change_review.users_id = params["review"]["users_id"].to_i
+  change_review.games_id = params["review"]["games_id"].to_i
+  change_review.score = params["review"]["score"].to_i
   
   change_review.save
   erb :"success/data_changed"
